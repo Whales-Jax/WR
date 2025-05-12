ee982878a0df98acccdb984bbb38ae50f4a2507e
Whales-Jax <lhz19950607@gmail.com>
2025-05-12 16:13:51 +0900

    Add files via upload

--- 1DHeatTransferFiniteVolumes.cpp
+++ 1DHeatTransferFiniteVolumes.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main()
 {
 	int p, i, j, N, M, X, n;
-	N = 5760;///// calculation times. N*dt= total time
+	N = 8640;///// calculation times. N*dt= total time
 	M = 1;/////
 	X = 200;
 
@@ -18,6 +18,7 @@ int main()
 	double  QQout, QQroom, qout, Qout, qroom, Qroom, L, dx, dt, t, Ti, Troom, Tset, Tout;
 	double  As, a, k, V, rho, hout, hroom, c;
 	double  Tp[400], T[400];
+	double  dT, Cair, Croom, Qac,Qin, kroom, Vair, cair, rhoair;
 
 	ofstream fout("1DwallConduction.csv");
 	if (!fout) {
@@ -32,28 +33,43 @@ int main()
 ////////////////////Simulation condition////////////////////
 
 	t = 0.0;				///////time [s]
-	dt = 30.0;				///////time step [s]
+	dt = 10.0;				///////time step [s]
 	Ti = 35.0;				///////initial wall temperature [oC]
 	Tout = 35.0;			///////external ambient temperature [oC]
 	Tset = 27.0;			///////room temperature [oC]
+	Troom = Tset;           ///////room temperature [oC] initialization
 	L = 0.20;				///////wall thickness [m]
 	dx = L / double(X);		///////mesh size [m]
 
-	///////////////wall properties//////////////////////////
+	///////////////wall properties////////////////////////// 
 
-	V = 20;				///////volume [m3]
+	V = 10;				///////volume [m3]
 	//k = 1.7;				///////thermal conductivity [J/m-K]
 	//rho = 2200.0;			///////density [kg/m3]
 	//c = 840.0;				///////specific heat [J/kg-K]
 	k = 1.2;				///////thermal conductivity [J/m-K]
-	rho = 2310;			///////density [kg/m3]
+	rho = 2310;			    ///////density [kg/m3]
 	c = 879;				///////specific heat [J/kg-K]
 	hout = 5.0;				///////external heat transfer coefficeint [W/m2-K]
 	hroom = 5.0;			///////internal heat transfer coefficeint [W/m2-K]
 	a = k / rho / c;		///////thermal diffusivity [m2/s]
-	As = 100.0;				///////heat transfer area [m2]
+	As = 45;				///////heat transfer area [m2]
+
+	///////////////air properties//////////////////////////
+
+	dT = 0;                 ///////Temperature change [oC] initialization
+	Vair = 27;              ///////volume of room air[m3]
+	rhoair = 1.2;           ///////density [kg/m3]
+	cair = 1005;            ///////specific heat [J/kg-K]
+	Cair = cair * Vair * rhoair;        ///////Heat capacity of room's air [J/K]
+	kroom = 1000;                ////////coefficient of Croom divide Cair      
+	Croom = kroom * Cair;      ///////Heat capacity of whole indoor space [J/K]
 
 	fout << t << ",";
+	fout << 0 << ",";
+	fout << 0 << ",";
+	fout << 0 << ",";
+	fout << 0 << ",";
 	fout << 0 << ",";
 	fout << 0 << ",";
 	fout << 0 << ",";
@@ -62,10 +78,17 @@ int main()
 	///////////////temperature profile initialization////////
 	for (p = 0; p <= X; p++) {
 
-		T[p] = Tout;
-		Tp[p] = Tout;
+		//T[p] = Tout;
+		//Tp[p] = Tout;
 		//T[p] = 32.0 - 2.0 / L * p * dx;
 		//Tp[p] = 32.0 - 2.0 / L * p * dx;
+		//T[p] = 32.178 - 11.765 * p * dx;           //this is for linear T distribution
+		//Tp[p] = 32.178 - 11.765 * p * dx;          //this is for linear T distribution
+
+		T[p] = 33.457 - 10.816 * p * dx;								//this is for another linear T distribution
+		Tp[p] = 33.457 - 10.816 * p * dx;								//this is for another linear T distribution
+		//T[p] = 27;									//this is for uniform T distribution
+		//Tp[p] = 27;									//this is for uniform T distribution
 		fout << T[p] << ",";
 
 	}
@@ -75,11 +98,13 @@ int main()
 
 	for (i = 1; i <= N; i++) {///////////////
 
+		
 		t = t + dt;
 		fout << t << ",";
 		n = i * dt / 1800;
 		//if (n % 2 == 0)
-			Troom = Tset;
+			//Troom = Tset;
+
 		//else Troom = 20.0;
 		//Troom = Tset + 1.0 * sin(2.0 * 3.14 * dt * double(i) / 4500.0);
 
@@ -163,6 +188,12 @@ int main()
 		fout << QQout << ",";
 		QQroom = hroom * As * (T[X] - Troom);
 		fout << QQroom << ",";
+
+		Qac = 0;										//air-conditioner heat flux (power)
+		Qin = Qroom + QQroom + Qac;						//heat flux to room in total
+		dT = Qin * dt / Croom;							//temperature change
+		Troom = Troom + dT;
+		fout << Qin << ",";
 		fout << Troom << ",";
 		///////////////assign next-time-step conditions////////
 		for (p = 0; p <= X; p++) {
